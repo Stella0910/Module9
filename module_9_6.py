3e84546749b36021eec2b1fc4363c4e8de28b614
Stella0910 <aniretake0910@gmail.com>
2024-12-02 20:05:33 +0300

    Add files via upload

--- module_9_6.py
+++ module_9_6.py
@@ -1,17 +1,13 @@
 def all_variants(text):
-    for i in range(len(text)):
-        var = text[i]
-        yield var
-    for i in range(len(text)-1):
-        var = text[i:i+2]
-        yield var
-    for i in range(len(text)-2):
-        var = text[i:]
-        yield var
-    for i in range(3, len(text)):
-        var = text[:i]
-        yield var
+    while len(text) > 1:
+        for i in range(1, len(text)):
+            var = text[:i]
+            yield var
+        for i in range(len(text)):
+            var = text[i:]
+            yield var
+        text = text[1:-1]
 
 
-for x in all_variants('abcde'):
+for x in all_variants('abcdefg'):
     print(x)
